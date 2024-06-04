d23db47d2bc3d6b9f62a35efd3da5bf87663f21d
Shulav Dangol <80204502+Shulav2002@users.noreply.github.com>
2024-06-05 00:01:14 +0545

    first commit

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -3,10 +3,10 @@
 import 'package:flutter/material.dart';
 
 class AppConstant {
-  static String appMainName = 'Ecomm';
-  static String appPoweredBy = 'Powered By Techi4u';
+  static String appMainName = 'Happy Roots';
+
   static String appVersion = '1.0.1';
-  static String whatsAppNumber = '+923075812354';
+
   static const appMainColor = Color.fromARGB(255, 87, 87, 87);
   static const appScendoryColor = Color.fromARGB(255, 23, 73, 36);
   static const appTextColor = Color(0xFFFBF5F4);
